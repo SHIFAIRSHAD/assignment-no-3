d5855431d2d0fa6c998ff4dc6c966486c79aed62
SHIFAIRSHAD <shifairshad36@gmail.com>
2025-04-19 15:12:26 -0700

    Add files via upload
    https://classroom.google.com/s

---
+++ Task.py
@@ -0,0 +1,9 @@
+
+a=int(input("enter the first number:  "))
+b=int(input("enter the second number: "))
+c=int(input("enter the third number:  "))
+print("Sum: ",a+b+c)
+print("Difference: ",a-b-c)
+print("product:",a*b*c)
+print("remainder:",a%b%c)
+
